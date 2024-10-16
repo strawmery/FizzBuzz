5485bd540e258e134967c0a85e1b960a974fc3a1
Maria Garcia Sierra <mgarciasierragrsr@gmail.com>
2024-10-16 19:33:59 +0200

    fizzbuzz method added

--- src/main/java/com/example/FizzBuzz.java
+++ src/main/java/com/example/FizzBuzz.java
@@ -2,5 +2,15 @@ package com.example;
 
 public class FizzBuzz 
 {
-
+    public static void fizzBuzz (int num){
+        if(num % 3 == 0){
+            System.out.println("FIZZ");
+        }else if (num % 5 == 0){
+            System.out.println("BUZZ");
+        }else if(num % 3 == 0 || num % 5 == 0){
+            System.out.println("FIZZBUZZ");
+        }else{
+            System.out.println("NADA");
+        }
+    }
 }
